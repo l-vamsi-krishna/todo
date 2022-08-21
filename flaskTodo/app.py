1a0c34cf2cc04f680e57c74b7ba0f9e841ab3ff2
Vamsi Krishna <krishnvamsi9010@gmail.com>
2022-08-21 23:27:58 +0530

    updated home page and added about page

--- flaskTodo/app.py
+++ flaskTodo/app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return "<p>Home Page</p>"
+
+@app.route("/about")
+def about():
+    return "<p>About Page</p>"
 
 if __name__=='__main__':
     app.run()
