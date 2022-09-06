31606fa86509e3c3e811b564eadd9de2905442b1
Vamsi Krishna <krishnvamsi9010@gmail.com>
2022-09-06 14:12:57 +0530

    base template added

--- flaskTodo/app.py
+++ flaskTodo/app.py
@@ -4,11 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html',title='Home')
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
-
+    return render_template('about.html',title='About')
 if __name__=='__main__':
     app.run()
