1fb17164984ca2a682b7d6c67b9abb8390dc693a
Vamsi Krishna <krishnvamsi9010@gmail.com>
2022-09-06 12:18:53 +0530

    html templates

--- flaskTodo/app.py
+++ flaskTodo/app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Home Page</p>"
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
-    return "<p>About Page</p>"
+    return render_template('about.html')
 
 if __name__=='__main__':
     app.run()
