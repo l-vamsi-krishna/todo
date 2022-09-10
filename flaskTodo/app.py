cc00af77b7bd30183f4b0a3893a8960ad187529d
Vamsi Krishna <krishnvamsi9010@gmail.com>
2022-09-10 18:33:55 +0530

    updated to use existing sql lite db

--- flaskTodo/app.py
+++ flaskTodo/app.py
@@ -1,6 +1,20 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db_path=r'C:\Users\krish\TODO\todo.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+db_path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    msg = db.Column(db.String)
+
+    def __repr__(self):
+        return f'{self.msg}'
+
+print(Todo.query.all())
 
 @app.route("/")
 def home():
